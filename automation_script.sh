aa8d50100b9ca3c419a0ffdf4b4f15fefb90e375
Omveer Panwar <apple@apples-MacBook-Pro.local>
2024-10-11 00:11:57 +0530

    Added automation scripts and screenshots

---
+++ automation_script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export ANDROID_HOME=/Users/apple/Library/Android/sdk
+export PATH=$ANDROID_HOME/emulator:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$PATH
+
+emulator -avd MyAndroid &  
+sleep 30  
+
+adb exec-out screencap -p > em_run.png
+
+adb shell am start -n org.mozilla.firefox/org.mozilla.firefox.App
+sleep 10  
+
+adb exec-out screencap -p > firefox_screenshot.png
+
+cat <<EOT > test.sh
+#!/bin/bash
+echo "Hi from Android!"
+EOT
+
+adb push test.sh /sdcard/
+
+adb shell <<EOF
+cd /sdcard/
+chmod 755 test.sh
+sh test.sh
+EOF
+
+adb exec-ut screencap -p > script_output.png
